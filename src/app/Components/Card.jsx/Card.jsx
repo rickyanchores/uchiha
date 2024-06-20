c047a73bac5885cf51b589bd51c35b18ebd2f632
RickyAnchores <ricky23anchores@gmail.com>
2024-06-20 09:22:08 +0200

    updated Gallery

--- src/app/Components/Card.jsx/Card.jsx
+++ src/app/Components/Card.jsx/Card.jsx
@@ -1,24 +1,17 @@
 // components/Card.js
 
 
-const Card = () => {
+const Card = ({image}) => {
   return (
-    <div className="Card py-4 shadow">
-      <div className="pb-0 pt-2 px-4 flex-col items-start">
-        <p className="text-tiny uppercase font-bold">Nxt Mix</p>
-        <small className="text-default-500">Sample 01</small>
-        <h4 className="font-bold text-large">Frontend Card</h4>
-      </div>
-      <div className="overflow-visible py-2">
+    <div className="Card">
         <img
           alt="Card background"
           className="object-cover"
-          src="https://zensushitogo.com/wp-content/uploads/2020/12/ZTG_TheBlackBoxPack_web.png"
-          width={370}
+          src={image}
+          width={420}
           height={150} // Ensure to add a height for the image to maintain the aspect ratio
         />
       </div>
-    </div>
   );
 };
 
--- src/app/Pages/Gallery/Gallery.jsx
+++ src/app/Pages/Gallery/Gallery.jsx
@@ -2,13 +2,31 @@ import Card from '@/app/Components/Card.jsx/Card';
 import React from 'react'
 
 const Gallery = () => {
+
+  let CardList = [
+    { 
+      id: 0,
+      title: "One",
+      image: "https://zensushitogo.com/wp-content/uploads/2020/12/ZTG_TheBlackBoxPack_web.png"
+    },
+    {
+      id: 1,
+      title: "One",
+      image: "https://cdn.concreteplayground.com/content/uploads/2023/08/Aoi-Tsuki-supplied-Griffin-Simm-credit-13.jpg"
+    },
+    {
+      id: 2,
+      title: "One",
+      image: "https://champ-magazine.com/wp-content/uploads/2023/09/Aman-Kyoto-Japan-Living-Pavilion_14.tif_27838-1-1280x0-c-default.jpg"
+    }
+  ]
+
   return (
     <div className='Gallery w-screen min-h-screen flex flex-col justify-center items-center' id='Gallery'>
-        <h1 className='text-4xl'>Gallery</h1>
         <div className="gallery-grid p-4 grid grid-cols-1 md:grid-cols-3 gap-4">
-          <Card />
-          <Card />
-          <Card />
+          {CardList.map((card) => (
+            <Card key={card.id} image={card.image}/>
+          ))}
         </div>
     </div>
   )
