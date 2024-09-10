adfa045145bf1a555e00e289253cccaf89c7bdd3
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2024-09-10 09:57:19 +0200

    update colors

--- src/app/Components/Nav/Nav.jsx
+++ src/app/Components/Nav/Nav.jsx
@@ -4,13 +4,13 @@ import React from 'react'
 const Nav = () => {
 
   return (
-    <div className='Nav shadow text-gray-700 font-semibold rounded-lg flex justify-between items-center p-4 mb-2  sticky top-2'>
-        <Link href="/#Hero"><h1 className='font-bold text-2xl text-red-700'>uchiha</h1></Link>
+    <div className='Nav shadow text-gray-700 font-semibold flex justify-between items-center p-4 mb-2  sticky top-2'>
+        <Link href="/#Hero"><h1 className='font-bold text-2xl text-red-900'>uchiha</h1></Link>
         <ul className="navLinks  flex justify-around items-center gap-4">
-          <Link href="/#Menu" className='hover:text-red-700'>Menu</Link>
-          <Link href="/#Gallery" className='hover:text-red-700'>Gallery</Link>
-          <Link href="/#About" className='hover:text-red-700'>About</Link>
-          <Link href="/#Faq" className='hover:text-red-700'>Faq</Link>
+          <Link href="/#Menu" className='hover:text-red-900'>Menu</Link>
+          <Link href="/#Gallery" className='hover:text-red-900'>Gallery</Link>
+          <Link href="/#About" className='hover:text-red-900'>About</Link>
+          <Link href="/#Faq" className='hover:text-red-900'>Faq</Link>
         </ul>
     </div>
   )
--- src/app/Pages/Hero/Hero.jsx
+++ src/app/Pages/Hero/Hero.jsx
@@ -4,10 +4,12 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='Hero opacity-2 w-screen min-h-screen' id='Hero' > 
-      <div className="Hero-container w-screen min-h-screen p-4 grid grid-cols-1 md:grid-cols-3 gap-4 justify-center items-center">
-        <p className='text-white'>Est 1993</p>
-        <h1 className='text-zinc-200'>Your new favorite restaurant</h1>
-        <h1 className='Title text-8xl font-bold uppercase text-white'>
+      <div className="Hero-container bg-red-900 w-screen min-h-screen p-4 grid grid-cols-1 md:grid-cols-2 gap-4 justify-center items-center">
+        <div className="hero-text">
+          <p className='text-white'>Est 1993</p>
+          <h1 className='text-zinc-200'>Your new favorite restaurant</h1>
+        </div>
+        <h1 className='Title text-8xl md:text-[9em] font-bold uppercase text-white'>
           uchiha
         </h1>
       </div>
